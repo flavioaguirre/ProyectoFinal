548d4eba9c7f6d2f1ee7be590e1265425d4bd737
Flavio Aguirre <flavioaguirre0@gmail.com>
2023-06-29 02:25:13 -0300

    Inicio-3(eleccion de la plantilla)

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inicio.views import .
+from inicio.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', )
+    path('', index),
 ]
